8f886129d5a672c5ce93c87624fab2b6ec2b874a
proJavaScriptDev <bodofocus11@gmail.com>
2021-01-26 17:59:03 +0530

    added constants to game js

--- utils/logic.js
+++ utils/logic.js
@@ -1,108 +1,27 @@
-// generate random number including min and max
+import {
+	THREE_WIN_PATTERNS,
+	FOUR_WIN_PATTERNS,
+	FIVE_WIN_PATTERNS,
+	THREE_ROBOT_TURNS,
+	FOUR_ROBOT_TURNS,
+	FIVE_ROBOT_TURNS,
+} from '../models/game'
 
-function randomBetween(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min)
-}
+// generate random number including min and max
 
 // returns the possible combinations that a player can win depending on the game_type (3x3, 4x4, 5x5)
 
-function winnerPatterns(game_type) {
+function getWinnerPatterns(game_type) {
 	let wins = Array()
 
 	// 3 x 3 winning patterns;
-	if (game_type === 3)
-		wins = [
-			[11, 12, 13],
-			[21, 22, 23],
-			[31, 32, 33],
-			[11, 21, 31],
-			[12, 22, 32],
-			[13, 23, 33],
-			[11, 22, 33],
-			[13, 22, 31],
-		]
+	if (game_type === 3) wins = THREE_WIN_PATTERNS
 
 	// 4 x 4 winning patterns;
-	if (game_type === 4)
-		wins = [
-			[11, 12, 13],
-			[12, 13, 14],
-			[21, 22, 23],
-			[22, 23, 24],
-			[31, 32, 33],
-			[32, 33, 34],
-			[41, 42, 43],
-			[42, 43, 44],
-			[11, 21, 31],
-			[21, 31, 41],
-			[12, 22, 32],
-			[12, 23, 34],
-			[22, 32, 42],
-			[13, 23, 33],
-			[13, 22, 31],
-			[23, 33, 43],
-			[14, 24, 34],
-			[24, 34, 44],
-			[14, 23, 32],
-			[23, 32, 41],
-			[11, 22, 33],
-			[22, 33, 44],
-			[21, 32, 43],
-			[24, 33, 42],
-		]
+	if (game_type === 4) wins = FOUR_WIN_PATTERNS
 
 	// 5 x 5 winning patterns;
-	if (game_type === 5)
-		wins = [
-			[11, 12, 13],
-			[12, 13, 14],
-			[13, 14, 15],
-			[21, 22, 23],
-			[22, 23, 24],
-			[23, 24, 25],
-			[31, 32, 33],
-			[32, 33, 34],
-			[33, 34, 35],
-			[41, 42, 43],
-			[42, 43, 44],
-			[43, 44, 45],
-			[51, 52, 53],
-			[52, 53, 54],
-			[53, 54, 55],
-			[11, 21, 31],
-			[21, 31, 41],
-			[31, 41, 51],
-			[12, 22, 32],
-			[12, 23, 34],
-			[23, 34, 45],
-			[22, 32, 42],
-			[32, 42, 52],
-			[13, 23, 33],
-			[13, 22, 31],
-			[13, 24, 35],
-			[23, 33, 43],
-			[33, 43, 53],
-			[14, 24, 34],
-			[24, 34, 44],
-			[34, 44, 54],
-			[14, 23, 32],
-			[23, 32, 41],
-			[15, 25, 35],
-			[25, 35, 45],
-			[35, 45, 55],
-			[11, 22, 33],
-			[22, 33, 44],
-			[33, 44, 55],
-			[21, 32, 43],
-			[32, 43, 54],
-			[24, 33, 42],
-			[33, 42, 51],
-			[15, 24, 33],
-			[25, 34, 43],
-			[34, 43, 52],
-			[35, 44, 53],
-			[31, 42, 53],
-		]
+	if (game_type === 5) wins = FIVE_WIN_PATTERNS
 	return wins
 }
 
@@ -111,60 +30,16 @@ function DefaultRobotPatterns(game_type) {
 	let robot_turns = Array()
 
 	// 3 x 3 winning patterns;
-	if (game_type === 3) robot_turns = [22, 11, 33, 13, 21, 23, 12, 32, 31]
+	if (game_type === 3) robot_turns = THREE_ROBOT_TURNS
 
 	// 4 x 4 winning patterns;
 	if (game_type === 4) {
-		robot_turns = [
-			randomBetween(21, 23),
-			randomBetween(31, 32),
-			randomBetween(33, 34),
-			34,
-			24,
-			43,
-			42,
-			41,
-			31,
-			21,
-			12,
-			13,
-			14,
-			44,
-			33,
-			randomBetween(22, 23),
-			11,
-		]
+		robot_turns = FOUR_ROBOT_TURNS
 	}
 
 	// 5 x 5 winning patterns;
 	if (game_type === 5) {
-		robot_turns = [
-			randomBetween(22, 24),
-			randomBetween(21, 23),
-			randomBetween(33, 34),
-			32,
-			43,
-			42,
-			54,
-			52,
-			randomBetween(53, 55),
-			25,
-			45,
-			35,
-			21,
-			31,
-			41,
-			51,
-			12,
-			13,
-			14,
-			15,
-			55,
-			44,
-			33,
-			randomBetween(22, 23),
-			11,
-		]
+		robot_turns = FIVE_ROBOT_TURNS
 	}
 	return robot_turns
 }
@@ -272,7 +147,7 @@ function getMostNearestPattern(board, symbol) {
 	const game_type = getGameType(board)
 
 	let selected = createSelectionArray(board, symbol)
-	let win_patterns = winnerPatterns(game_type)
+	let win_patterns = getWinnerPatterns(game_type)
 
 	finished = false
 	for (let x = 0; x < win_patterns.length; x++) {
@@ -319,7 +194,7 @@ exports.getRobotMove = (board, symbol) => {
 
 exports.checkGameWin = (board, symbol) => {
 	let game_type = getGameType(board)
-	let win_patterns = winnerPatterns(game_type)
+	let win_patterns = getWinnerPatterns(game_type)
 	for (let x = 0; x < win_patterns.length; x++) {
 		const win = isWinner(game_type, win_patterns[x], board, symbol)
 		if (win) {
