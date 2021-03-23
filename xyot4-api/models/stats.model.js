1d37218dfcb925b6817cef9db4c226dc79fe60fa
proJavaScriptDev <bodofocus11@gmail.com>
2021-03-23 20:36:28 +0530

    stats

--- xyot4-api/models/stats.model.js
+++ xyot4-api/models/stats.model.js
@@ -1,5 +1,5 @@
 const pool = require('../connection')
-const LEADERBOARD_TABLE = 'leaderboard'
+const STATS_TABLE = 'stats'
 const { GameTypes } = require('../../models/rooms')
 const USER_TABLE = 'users'
 
@@ -7,7 +7,7 @@ const Stats = {
 	async createNew(userId) {
 		const level = 0
 		const wins = 0
-		let query = `INSERT INTO ${LEADERBOARD_TABLE} (user_id, wins, level) VALUES(?, ?, ?)`
+		let query = `INSERT INTO ${STATS_TABLE} (user_id, wins, level) VALUES(?, ?, ?)`
 		const [result] = await pool.execute(query, [userId, wins, level])
 		if (result.affectedRows > 0) {
 			/* return the inserted data */
@@ -19,13 +19,13 @@ const Stats = {
 		let query
 		switch (gameType) {
 			case GameTypes.TWO_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET two_player_draws = two_player_draws + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET two_player_draws = two_player_draws + 1 WHERE user_id = ?`
 				break
 			case GameTypes.THREE_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET three_player_draws = three_player_draws + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET three_player_draws = three_player_draws + 1 WHERE user_id = ?`
 				break
 			case GameTypes.FOUR_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET four_player_draws = four_player_draws + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET four_player_draws = four_player_draws + 1 WHERE user_id = ?`
 				break
 			default:
 				query = ''
@@ -41,13 +41,13 @@ const Stats = {
 		let query
 		switch (gameType) {
 			case GameTypes.TWO_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET two_player_losses = two_player_losses + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET two_player_losses = two_player_losses + 1 WHERE user_id = ?`
 				break
 			case GameTypes.THREE_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET three_player_losses = three_player_losses + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET three_player_losses = three_player_losses + 1 WHERE user_id = ?`
 				break
 			case GameTypes.FOUR_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET four_player_losses = four_player_losses + 1 WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET four_player_losses = four_player_losses + 1 WHERE user_id = ?`
 				break
 			default:
 				query = ''
@@ -68,13 +68,17 @@ const Stats = {
             Level 2 : need 15 wins more i.e total 25 wins.
             Level 3 : need 20 wins more i.e total 45 wins. And so on increase by 5
         */
-		let query = `SELECT SUM(two_player_wins, three_player_wins, four_player_wins) AS total_wins FROM ${LEADERBOARD_TABLE} WHERE user_id=?`
+		let query = `SELECT two_player_wins, three_player_wins, four_player_wins FROM ${STATS_TABLE} WHERE user_id=?`
 		const [rows, fields] = await pool.execute(query, [userId])
 		let level, totalWins
 
 		if (rows.length > 0) {
 			/* calculate level */
-			totalWins = rows[0].total_wins + 1
+			totalWins =
+				rows[0].two_player_wins +
+				rows[0].three_player_wins +
+				rows[0].four_player_wins +
+				1
 			function getWins(level) {
 				/* returns the minimum wins required to reach that level */
 				/* sum of arithmetic sequence (n * (2a + (n-1)d)) / 2 */
@@ -91,13 +95,13 @@ const Stats = {
 		}
 		switch (gameType) {
 			case GameTypes.TWO_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET two_player_wins = two_player_wins + 1, level = ? WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET two_player_wins = two_player_wins + 1, level = ? WHERE user_id = ?`
 				break
 			case GameTypes.THREE_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET three_player_wins = three_player_wins + 1, level = ? WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET three_player_wins = three_player_wins + 1, level = ? WHERE user_id = ?`
 				break
 			case GameTypes.FOUR_PLAYER:
-				query = `UPDATE ${LEADERBOARD_TABLE} SET four_player_wins = four_player_wins + 1, level = ? WHERE user_id = ?`
+				query = `UPDATE ${STATS_TABLE} SET four_player_wins = four_player_wins + 1, level = ? WHERE user_id = ?`
 				break
 		}
 		const [result] = await pool.execute(query, [level, userId])
@@ -109,7 +113,7 @@ const Stats = {
 		return null
 	},
 	async getStats(userId) {
-		let query = `SELECT * FROM ${LEADERBOARD_TABLE} WHERE user_id = ?`
+		let query = `SELECT * FROM ${STATS_TABLE} WHERE user_id = ?`
 		const [rows, fields] = await pool.execute(query, [userId])
 		if (rows.length > 0) {
 			return rows[0]
@@ -118,7 +122,7 @@ const Stats = {
 	},
 	async getLeaderboard() {
 		/* returns the top 100 players */
-		let query = `SELECT u.name, l.*, SUM(two_player_wins, three_player_wins, four_player_wins) AS total_wins FROM ${LEADERBOARD_TABLE} AS l INNER JOIN ${USER_TABLE} AS u ON l.user_id = u.id ORDER BY total_wins`
+		let query = `SELECT u.name, l.*, SUM(two_player_wins, three_player_wins, four_player_wins) AS total_wins FROM ${STATS_TABLE} AS l INNER JOIN ${USER_TABLE} AS u ON l.user_id = u.id ORDER BY total_wins`
 		const [rows, fields] = await pool.execute(query)
 		if (rows.length > 0) {
 			return rows
