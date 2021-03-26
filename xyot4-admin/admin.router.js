34d2295657134a7b10fdb8b0c20316e97c30275e
proJavaScriptDev <bodofocus11@gmail.com>
2021-03-26 19:34:40 +0530

    added xyot login

--- xyot4-admin/admin.router.js
+++ xyot4-admin/admin.router.js
@@ -3,9 +3,18 @@ const router = express.Router()
 const rootRouter = express.Router()
 const viewController = require('./controllers/view.controller')
 
+router.get('/login', viewController.getLogin)
+router.post('/login', viewController.loginAdmin)
+router.post('/logout', viewController.logoutAdmin)
+
+/* protected routes */
+
+router.use(viewController.protect)
+
 router.get('/', viewController.getIndex)
 router.get('/users', viewController.getUsers)
 router.get('/users/:userId', viewController.getUser)
+
 router.post('/users/delete/:userId', viewController.deleteUser)
 
 router.get('/leaderboard', viewController.getLeaderboard)
--- xyot4-admin/controllers/view.controller.js
+++ xyot4-admin/controllers/view.controller.js
@@ -1,8 +1,17 @@
 const User = require('../../xyot4-api/models/user.model')
 const Stats = require('../../xyot4-api/models/stats.model')
 
+exports.protect = (req, res, next) => {
+	/* middleware to only allow logged in routes */
+	if (req.session.isLoggedIn) next()
+	else res.redirect('/admin/login')
+}
 exports.getIndex = (req, res) => {
 	res.render('admin/index')
+}
+exports.getLogin = (req, res) => {
+	if (req.session.isLoggedIn) res.redirect('/admin')
+	res.render('admin/login')
 }
 exports.getUsers = async (req, res) => {
 	const users = await User.getUsers()
@@ -19,13 +28,32 @@ exports.getUser = async (req, res) => {
 		backurl,
 	})
 }
+exports.getLeaderboard = async (req, res) => {
+	const leaderboard = await Stats.getLeaderboard()
+	res.render('admin/leaderboard', { leaderboard })
+}
+
+/* post requests */
+exports.loginAdmin = (req, res) => {
+	const { username, password } = req.body
+	if (
+		username === process.env.ADMIN_USERNAME &&
+		password === process.env.ADMIN_PASSWORD
+	) {
+		/* store the session after successful login */
+		req.session.isLoggedIn = true
+	}
+
+	/* attempt redirection to admin panel */
+	res.redirect(`/admin`)
+}
+exports.logoutAdmin = (req, res) => {
+	req.session.destroy()
+	res.redirect(`/admin/login`)
+}
 exports.deleteUser = async (req, res) => {
 	const { userId } = req.params
 	const { redirect_url: redirectUrl } = req.query
 	await User.deleteUser(userId)
 	res.redirect(`/admin/${redirectUrl}`)
-}
-exports.getLeaderboard = async (req, res) => {
-	const leaderboard = await Stats.getLeaderboard()
-	res.render('admin/leaderboard', { leaderboard })
 }
